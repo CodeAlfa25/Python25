0727374458fce386b225e5470924f5c70b108686
CodeAlfa25 <codealfa25@gmail.com>
2025-07-21 16:28:51 +0530

    Created A Simple Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+import random
+a = input("Enter First Number : ")
+b = input("Enter Second Number : )
+c = input("Enter The Method Of Calculation : ")
+
+if c == "+":
+  print(a+b)
+
+if c == "-":
+  print(a-b)
+
+if c == "*":
+  print(a*b)
+
+if c == "\":
+  print(a\b)
+
+if c == "%":
+  print(a%b)
+
+if c == "PubliusIsTheGoat":
+  print("Congratulations You have Made CodeAlfa25 Happy")
+
+else:
+  print("Please Enter A Valid Method Of Calculation")
